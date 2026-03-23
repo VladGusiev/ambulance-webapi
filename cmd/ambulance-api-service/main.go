0d0f2424cb5fa03aa79dbb89d1f71b07339e8ebd
Vladyslav Gusiev <vladyslav.gusiev@gmail.com>
2026-03-23 12:33:47 +0100

    ambulance waiting list

--- cmd/ambulance-api-service/main.go
+++ cmd/ambulance-api-service/main.go
@@ -8,6 +8,12 @@ import (
 	"github.com/VladGusiev/ambulance-webapi/api"
 	"github.com/VladGusiev/ambulance-webapi/internal/ambulance_wl"
 	"github.com/gin-gonic/gin"
+
+	"context"
+	"time"
+
+	"github.com/VladGusiev/ambulance-webapi/internal/db_service"
+	"github.com/gin-contrib/cors"
 )
 
 func main() {
@@ -22,11 +28,30 @@ func main() {
 	}
 	engine := gin.New()
 	engine.Use(gin.Recovery())
+
+	corsMiddleware := cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "PUT", "POST", "DELETE", "PATCH"},
+		AllowHeaders:     []string{"Origin", "Authorization", "Content-Type"},
+		ExposeHeaders:    []string{""},
+		AllowCredentials: false,
+		MaxAge:           12 * time.Hour,
+	})
+
+	// setup context update  middleware
+	dbService := db_service.NewMongoService[ambulance_wl.Ambulance](db_service.MongoServiceConfig{})
+	defer dbService.Disconnect(context.Background())
+	engine.Use(func(ctx *gin.Context) {
+		ctx.Set("db_service", dbService)
+		ctx.Next()
+	})
+	engine.Use(corsMiddleware)
 	// request routings
 
 	handleFunctions := &ambulance_wl.ApiHandleFunctions{
 		AmbulanceConditionsAPI:  ambulance_wl.NewAmbulanceConditionsApi(),
 		AmbulanceWaitingListAPI: ambulance_wl.NewAmbulanceWaitingListApi(),
+		AmbulancesAPI:           ambulance_wl.NewAmbulancesApi(),
 	}
 	ambulance_wl.NewRouterWithGinEngine(engine, *handleFunctions)
 	engine.GET("/openapi", api.HandleOpenApi)
--- internal/ambulance_wl/README.md
+++ internal/ambulance_wl/README.md
@@ -13,7 +13,7 @@ To see how to make this your own, look here:
 [README](https://openapi-generator.tech)
 
 - API version: 1.0.0
-- Build date: 2026-03-23T10:34:09.634790968Z[Etc/UTC]
+- Build date: 2026-03-23T11:02:57.097482670Z[Etc/UTC]
 - Generator version: 7.21.0-SNAPSHOT
 
 ### Running the server
