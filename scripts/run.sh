df2264da30e5c450466f962b109e54ab2b7c184e
Vladyslav Gusiev <vladyslav.gusiev@gmail.com>
2026-03-23 11:56:57 +0100

    Add mongodb compose file

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -euo pipefail
 
 command_name="${1:-start}"
 
@@ -7,10 +6,21 @@ project_root="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
 export AMBULANCE_API_ENVIRONMENT="Development"
 export AMBULANCE_API_PORT="8080"
+export AMBULANCE_API_MONGODB_USERNAME="root"
+export AMBULANCE_API_MONGODB_PASSWORD="secret"
+
+mongo() {
+  docker compose --file "${project_root}/deployments/docker-compose/compose.yaml" "$@"
+}
 
 case "$command_name" in
   start)
+    mongo up --detach
+    trap 'mongo down' EXIT
     go run "${project_root}/cmd/ambulance-api-service"
+    ;;
+  mongo)
+    mongo up
     ;;
   openapi)
     docker run --rm -ti -v "${project_root}:/local" openapitools/openapi-generator-cli generate -c /local/scripts/generator-cfg.yaml
